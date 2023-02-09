dd70502a87481c2f623b1ad61b1e127bff194d3d
laurapascual <laurapascualmateo@gmail.com>
2023-02-09 11:11:11 +0100

    add object

--- js/main.js
+++ js/main.js
@@ -2,31 +2,44 @@
 const newFormElement = document.querySelector('.js-new-form');
 
 let kittenList= document.querySelector(".js-list");
-function renderKitten(url, desc, name, race) {
-    return `<li class="card"> <img class="card_img" src="${url}" alt="gatito"/> <h3 class="card_title"> ${name} </h3> <h4 class="card_race"> ${race} </h4> <p class="card_description"> ${desc} </p> </li>`;
-} 
 
-const kittenImage = 'https://dev.adalab.es/gato-siames.webp';
-const kittenName = 'Anastacio';
-const kittenRace = 'Siamés';
-const kittenDesc =
-  ' Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
+const renderKitten = (kittenData) => {
+    const html =
+     `<li class="card"> 
+     <img class="card_img" src="${kittenData.image}" alt="gatito"/> 
+     <h3 class="card_title"> ${kittenData.name} </h3> 
+     <h4 class="card_race"> ${kittenData.race} </h4> 
+     <p class="card_description"> ${kittenData.desc} </p> 
+     </li>`;
+     return html;
+}
 
-const kittenOne = renderKitten(kittenImage, kittenDesc , kittenName, kittenRace);
+const kittenData_1 = {
+  image: 'https://dev.adalab.es/gato-siames.webp',
+  name: 'Anastacio',
+  desc: ' Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.',
+  race: 'Siamés',
+};
 
-const kittenImageTwo = 'https://dev.adalab.es/sphynx-gato.webp';
-const kittenNameTwo = 'Fiona';
-const kittenRaceTwo = 'Sphynx';
-const kittenDescTwo =
-  ' Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.';
-const kittenTwo = renderKitten(kittenImageTwo, kittenDescTwo , kittenNameTwo, kittenRaceTwo);
+kittenList.innerHTML += renderKitten(kittenData_1);
 
-const kittenImageThree = 'https://dev.adalab.es/maine-coon-cat.webp';
-const kittenNameThree = 'Cielo';
-const kittenRaceThree = 'Maine Coon';
-const kittenDescThree =
-  ' Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.';
-const kittenThree = renderKitten(kittenImageThree, kittenDescThree , kittenNameThree, kittenRaceThree);
+const kittenData_2 = {
+  image: 'https://dev.adalab.es/sphynx-gato.webp',
+  name: 'Fiona',
+  desc: ' Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.',
+  race: 'Sphynx',
+};
+
+kittenList.innerHTML += renderKitten(kittenData_2);
+
+const kittenData_3 = {
+  image: 'https://dev.adalab.es/maine-coon-cat.webp',
+  name: 'Cielo',
+  desc: ' Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.',
+  race: 'Maine Coon',
+};
+
+kittenList.innerHTML += renderKitten(kittenData_3);
 
 const buttonSearch = document.querySelector('.js-button-search');
 const input_search_desc = document.querySelector('.js_in_search_desc');
@@ -35,16 +48,16 @@ const filterKitten = (event) => {
   event.preventDefault();
   kittenList.innerHTML = "";
   const descrSearchText = input_search_desc.value;
-if( kittenDesc.includes(descrSearchText) ) {
-  kittenList.innerHTML += kittenOne;
+if( kittenData_1.desc.includes(descrSearchText) ) {
+  kittenList.innerHTML += renderKitten(kittenData_1);
 }
 
-if( kittenDescTwo.includes(descrSearchText) ) {
-  kittenList.innerHTML += kittenTwo;
+if( kittenData_2.desc.includes(descrSearchText) ) {
+  kittenList.innerHTML += renderKitten(kittenData_2);
 }
 
-if( kittenDescThree.includes(descrSearchText) ) {
-  kittenList.innerHTML += kittenThree;
+if( kittenData_3.desc.includes(descrSearchText) ) {
+  kittenList.innerHTML += renderKitten(kittenData_3);
 }
 }
 
